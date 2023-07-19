d11078e7b24282a655c6b7c23ccac31f09a95453
Christos Riggas <124679358+ChristosRiggas@users.noreply.github.com>
2023-07-19 15:55:32 +0300

    Add files via upload

--- mobile.php
+++ mobile.php
@@ -58,10 +58,11 @@
 		
 	<div id="content">		
 		<input class="fire-button" type="button" value="Fire" onclick="fire()">
-		<img style="width: 100px; position: fixed; display: block; margin: auto; right: 0px; margin-top: 43vh;" src="style/images/icons/full-screen-38-48.png" alt="ToggleFullscreen" id="fullScreenButton" onclick="toggleFullscreen();">
+		<img style="width: 100px; position: absolute; margin: auto; right: 0px; top: 44%;" src="style/images/icons/full-screen-38-48.png" alt="ToggleFullscreen" id="fullScreenButton" onclick="toggleFullscreen();">
 		<h2 id="playerCurrentSlot"></h2>
 		<input class="block-button" type="button" value="Block" onclick="block()">
-		
+		<input class="disconnect-button" style="text-align: center; margin: auto; font-size: 20px; position: absolute; top: 48%; transform: translateY(-50%); transform: rotate(90deg); background-color: #F0F0F0; padding: 6px 10px; box-sizing: border-box; border: 3px solid #ccc; border-radius: 4px;" type="button" value="Disconnect" onclick="disconnect()">
+
 <!-- 		<iframe id ="iframe" src="game/game.html" title="P5 Game"></iframe> -->
 		
 <!-- 		<h3 id="mobileDebug"><h3>
@@ -288,6 +289,34 @@
 			//document.getElementById('roll').innerHTML = 'roll ' + roll;
 			//document.getElementById('yaw').innerHTML = 'yaw ' + yaw;	
 		}
+			
+		// disconnect Player
+		function disconnect()
+		{	
+			let text = "Are you sure you want to disconnect?";
+			if (confirm(text) == true) {
+				let xmlhttpId = new XMLHttpRequest();				
+				xmlhttpId.onreadystatechange = function() {
+					if (xmlhttpId.readyState == XMLHttpRequest.DONE) {
+						if (xmlhttpId.status == 200) {
+							alert('You have been disconnected from slot ' + currentSlot);
+							document.cookie = "loggedIn=0; expires=Thu, 18 Dec 2013 12:00:00 UTC";
+							location.reload();					
+						}		
+						else if (xmlhttpId.status == 400) {
+								alert('There was an error 400');
+						}
+						else {
+							alert('something else other than 200 was returned');
+						}
+					}			
+				};
+				xmlhttpId.open("GET", "php/create_player.php?slot="+currentSlot+"&disconnect=dis", true);
+				xmlhttpId.send();						
+			}else {
+				text = "You canceled!";
+			}
+		}
 		
 		function testThisSlot(thisSlot)
 		{
@@ -301,6 +330,7 @@
 						if (xmlSlots.status == 200) {
 							try{
 								let slots = JSON.parse(xmlSlots.responseText);
+								console.log(xmlSlots.responseText);
 								if(slots.current == slot){
 									currentSlot = slots.current;
 									document.getElementById('playerCurrentSlot').innerHTML = "Player: " + currentSlot;
@@ -320,6 +350,7 @@
 												alert('You are Player ' + currentSlot);
 												document.getElementById('playerForm').style.display = "none"; 
 												//document.getElementById('responseDebugCreatePlayer').innerHTML = xmlhttp.responseText;
+												message_send = false;
 											}else if (xmlhttp.status == 400) {
 												alert('There was an error 400');
 											}else {
@@ -330,15 +361,22 @@
 									xmlhttp.open("GET", "php/create_player.php?slot="+currentSlot+"&name="+encodeURIComponent(name)+"&character="+encodeURIComponent(character), true);
 									xmlhttp.send();
 									
-									alert("Player data sent");
-								}else if(slots.current = "taken"){
+									//alert("Player data sent");
+								}else if(slots.current == "taken"){
 									alert('Slot ' + slot + ' is taken');
 									document.cookie = 'loggedIn=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
-									document.getElementById('otherSlots').style.display = "block"; 	
-									document.getElementById('playerForm').style.display = "none"; 								
+									OtherSlots();								
+								}else if(slots.current == "endround"){ // if the round is ended then show the correct message
+									document.cookie = 'loggedIn=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+									alert("Round has ended, please wait for the next round");
+									document.getElementById('content').style.display = "none";
+									OtherSlots();							
 								}
+								
 							}catch(err){
-								window.location.href = 'mobile.php';
+								location.reload();
+								//window.location.href = 'mobile.php';
+								//console.log(xmlSlots.responseText);
 							}
 						}
 						else if (xmlSlots.status == 400) {
@@ -365,18 +403,6 @@
 
 		function sendData()
 		{
-/* 			let playersNumberCreated;
-			let playersNumberInstallation;
-			readPlayersNum('php/data/players_created_counter.data')
-				.then(dataText => {
-					playersNumberCreated = dataText;
-			});
-			
-			readPlayersNum('php/data/installation_form_app.data')
-				.then(dataText => {
-					playersNumberInstallation = dataText;
-			}); */
-			
 			if(parseInt(currentSlot) > -1 /* && (parseInt(playersNumberCreated) == parseInt(playersNumberInstallation) - 1) */){
 				// test if the user is a valid one (by checking his/her id) before sending the data
 				let xmlhttpId = new XMLHttpRequest();				
@@ -423,10 +449,11 @@
 														console.log(roundData);
 														
 														endRound = roundData.endround;
-														if(endRound == "true"){
+														if(!message_send && endRound == "true"){
 															alert("Round has ended, please wait for the next round");
 															document.getElementById('content').style.display = "none";
 															OtherSlots();
+															message_send = true;
 														}
 
 														if(roundData["tookdmg" + parseInt(currentSlot)] == "true"){
@@ -442,22 +469,6 @@
 															stopCurrentAudioSample();	
 															audioSamples[parseInt(type)].play();												
 														}
-														
-/*  														let xmlhttpRoundDataReset = new XMLHttpRequest();
-														xmlhttpRoundDataReset.onreadystatechange = function(){
-															if (xmlhttpRoundDataReset.readyState == XMLHttpRequest.DONE) {
-																if (xmlhttpRoundDataReset.status == 200) {
-
-																}else if (xmlhttpRoundDataReset.status == 400) {
-																	alert('There was an error 400');
-																}else {
-																	alert('something else other than 200 was returned');
-																}
-															} 
-														};
-														xmlhttpRoundDataReset.open("GET", "php/round_data.php?data=reset", true);
-														xmlhttpRoundDataReset.send(); */
-			
 													}
 
 												}else if (xmlhttpRoundData.status == 400) {
@@ -472,13 +483,14 @@
 										
 										
 									}else if(response.valid == false){	
-										if(!message_send && endRound){
+										if(!message_send && endRound == "true"){
 											alert("Disconnected due to invalid player data or because round ended");
 											document.getElementById('otherSlots').style.display = "none";		
 											location.reload();
 											message_send = true;
 										}
-										window.location.href = 'mobile.php';
+										//window.location.href = 'mobile.php';
+										location.reload();
 										fireTrue = 0;
 										blockTrue = 0;	
 									} 
--- php/check_slots.php
+++ php/check_slots.php
@@ -3,33 +3,57 @@
 	
 	//reset all the slots, players created counter and their data
 	if(isset($_GET["reset"]) && $_GET["reset"] == 100){
-		$fileSlots = fopen('data/slots.data', "w");	
+		$fileSlots = fopen("data/slots.data", "w");	
+		$filePlayersToDisconnect = fopen("data/players_to_disconnect.data", "w");	
 		for($i = 0; $i < $playersNum; $i++){
 			if($i != $playersNum - 1){
 				fwrite($fileSlots, 1 . "&");
+				fwrite($filePlayersToDisconnect, 0 . "&");
 			}else{			
-				fwrite($fileSlots, 1);			
+				fwrite($fileSlots, 1);
+				fwrite($filePlayersToDisconnect, 0);				
 			}
 		}
 		fclose($fileSlots);		
+		fclose($filePlayersToDisconnect);
 		
 		file_put_contents('data/mobile_app.data', "");
 		file_put_contents('data/players_created_counter.data', strval(-1)); // used and as index so starts to count from 0
 		file_put_contents('data/create_player_app.data', strval(""));
-		file_put_contents('data/round_app.data', strval(""));
-		unlink('data/round_app.data');
+		//file_put_contents('data/round_app.data', "");
+		//unlink('data/round_app.data');
 
 	// slot check
 	}else if(isset($_GET["slotTemp"])){
 		$data = file_get_contents('data/slots.data');
 		$data = explode("&", $data);
 		
-		if($data[$_GET["slotTemp"]] == 1){
+		// check also if endround = false, if there is available game
+		$roundData = file_get_contents('data/round_app.data');
+		$finalRoundDataArrayArray = [];
+ 		if(!empty($roundData)){
+			$roundDataArray = explode("&", $roundData);
+			
+			foreach ($roundDataArray as $element){
+				$tempArray = explode(":", $element);
+				$key = $tempArray[0];
+				
+				$finalRoundDataArrayArray[$key] = $tempArray[1];
+			}
+		} 
+		
+		//echo "finalRound: ".implode(",", $finalRoundDataArrayArray)."<br>";
+		//echo strval($finalRoundDataArrayArray["endround"])."<br>";
+
+		if($data[$_GET["slotTemp"]] == 1 && $finalRoundDataArrayArray["endround"] == "false"){
 			echo json_encode(array('current' => $_GET["slotTemp"]));
 			$data[$_GET["slotTemp"]] = 0;
 			$data = join("&", $data);
 			file_put_contents('data/slots.data', strval($data));
-		}else{
+
+		}else if($finalRoundDataArrayArray["endround"] == "true"){
+			echo json_encode(array('current' => 'endround'));
+		}else if($data[$_GET["slotTemp"]] == 0 && $finalRoundDataArrayArray["endround"] == "false"){
 			echo json_encode(array('current' => 'taken'));
 		}	
 	} 
